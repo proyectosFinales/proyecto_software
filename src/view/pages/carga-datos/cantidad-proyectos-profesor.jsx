783f2a2f8b53fea8b4c4d7f493c3bf6641903cf9
Roggenrolo <rmcordero.99@gmail.com>
2024-10-04 11:28:48 -0600

    edición de cantidad de proyectos por profesor

--- src/view/pages/carga-datos/cantidad-proyectos-profesor.jsx
+++ src/view/pages/carga-datos/cantidad-proyectos-profesor.jsx
@@ -1,70 +1,59 @@
-import { useRef, useState } from "react";
-import * as XLSX from "xlsx";
+// import * as XLSX from "xlsx";
 import Button from "../../components/button";
-import Modal from "../../components/modal";
-import { FloatInput } from "../../components/input";
 import Layout from "../../components/layout";
+import styles from "../../styles/table.module.css";
+import { FloatInput } from "../../components/input";
+
+// const handleExcelChange = event => {
+//     const file = event.target.files[0];
+//     if (file) {
+//         const reader = new FileReader();
+//         reader.onload = (event) => {
+//             const binaryStr = event.target.result;
+//             // Leer el archivo Excel como binario
+//             const workbook = XLSX.read(binaryStr, {type: "binary"});
+//             // Leer la primera hoja del archivo
+//             const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+//             // Convertir los datos a JSON
+//             const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+//             setExcelData({
+//                 titles: data[0],
+//                 data: data.slice(1).filter(d => d.length > 0),
+//                 uploaded: true
+//             });
+//         };
+//         // Leer el archivo como binario
+//         reader.readAsBinaryString(file);
+//     }
+// }
 
 const CantidadProyectosProfesor = () => {
-    const modalRef = useRef();
-    const [excelData, setExcelData] = useState({titles: [], data: [], uploaded: false});
-
-    const handleExcelChange = event => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const binaryStr = event.target.result;
-                // Leer el archivo Excel como binario
-                const workbook = XLSX.read(binaryStr, {type: "binary"});
-                // Leer la primera hoja del archivo
-                const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-                // Convertir los datos a JSON
-                const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-                setExcelData({
-                    titles: data[0],
-                    data: data.slice(1).filter(d => d.length > 0),
-                    uploaded: true
-                });
-            };
-            // Leer el archivo como binario
-            reader.readAsBinaryString(file);
-        }
-    }
+    const profesores = Array(10).fill({ nombre: "Johanna Madrigal", cantidadProyectos: 3})
 
     return <>
         <Layout title="Asignación de cantidad de proyectos por profesor">
-            <FloatInput text="Ingrese el archivo de excel">
-                <input type="file" accept=".xlsx" onChange={handleExcelChange} />
-            </FloatInput>
-            <center>
-                <Button onClick={() => modalRef.open()} type="dark">Subir datos</Button>
-            </center>
-            {excelData.uploaded && 
-                <div style={{marginTop: "20px"}}>Cantidad de filas: {excelData.data.length}</div>
-            }
-            <section>
-                {excelData.data.map((row, i) =>
-                    <ul key={`row-${i}`}>
-                        {row.map((data, j) =>
-                            <li key={`cell-${i}-${j}`}>
-                                <b>{excelData.titles[j]}: </b> {data}
-                            </li>
-                        )}
-                        <hr/>
-                    </ul>
-                )}
-            </section>
+            <Button type="dark">Guardar cambios</Button>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Profesor</th>
+                        <th>Cantidad de proyectos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {profesores.map((profesor, i) =>
+                        <tr key={`profesor-${i}`}>
+                            <td>{profesor.nombre}</td>
+                            <td>
+                                <FloatInput text="">
+                                    <input type="number" value={profesor.cantidadProyectos} onChange={()=>{}}/>
+                                </FloatInput>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </Layout>
-        <Modal
-            title="Confirmación"
-            modalRef={modalRef}
-            footer={<Button type="dark">Confirmar</Button>}
-        >
-            <b style={{fontSize: "20px", margin: "0"}}>
-                Está seguro de subir los datos de cantidad de proyectos por profesor en el archivo especificado?
-            </b>
-        </Modal>
     </>;
 }
 
