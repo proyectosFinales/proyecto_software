956a585d0b08acca51c7cbf8cdd6193b9e39c882
Roggenrolo <rmcordero.99@gmail.com>
2024-10-19 15:38:21 -0600

    visualización de asignación de anteproyecto

--- src/controller/asignacion.js
+++ src/controller/asignacion.js
@@ -8,17 +8,41 @@ import Profesor from "./profesor";
  */
 const asignarAnteproyectosAProfesores = (anteproyectos, profesores) => {
     return new Promise(resolve => {
+        const asignados = [];
+        const noasignados = []
+        let indiceProfesor = 0;
+        // Iteramos anteproyectos uno a uno con profesores para lograr una asignación nivelada
         anteproyectos.forEach(anteproyecto => {
-            profesores.forEach(profesor => {
-                // Se comprueba disponibilidad del profesor
-                if(profesor.cantidadProyectos === 0) return;
-                // Sede
-                if(anteproyecto.estudiante.sede !== profesor.sede) return;
-                // Repitente
-            });
+            for(let i=0; i<profesores.length; i++) {
+                const profesor = profesores[indiceProfesor];
+                // Iterar circularmente a profesores
+                indiceProfesor = indiceProfesor == profesores.length - 1 ? 0 : indiceProfesor + 1;
+                
+                if(esAsignable(anteproyecto, profesor)) {
+                    anteproyecto.encargado = profesor;
+                    asignados.push(anteproyecto);
+                    return;
+                }
+            }
+            noasignados.push(anteproyecto);
         });
-        // Repitente
+        resolve({asignados, noasignados});
     });
+}
+
+/**
+ * Confirma si un anteproyecto es asignable a un profesor
+ * @param {Anteproyecto} anteproyecto 
+ * @param {Profesor} profesor 
+ */
+const esAsignable = (anteproyecto, profesor) => {
+    // Se comprueba disponibilidad del profesor
+    if(profesor.cantidadProyectos === 0) return false;
+    // Sede
+    if(anteproyecto.estudiante.sede !== profesor.sede) return false;
+    // Repitente con el mismo profesor
+    if(anteproyecto.anteproyectosPerdidos.find(ap => ap.encargado.id === profesor.id)) return false;
+    return true;
 }
 
 export { asignarAnteproyectosAProfesores };
