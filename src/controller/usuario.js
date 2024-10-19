7f4c6d8fa64c6598d8acde6491185b6497197278
Roggenrolo <rmcordero.99@gmail.com>
2024-10-19 15:38:21 -0600

    visualización de asignación de anteproyecto

--- src/controller/usuario.js
+++ src/controller/usuario.js
@@ -4,6 +4,11 @@ class Usuario {
         this.nombre = nombre;
         this.sede = sede;
     }
+
+    static from (obj) {
+        if(obj == null) return null;
+        return new Usuario(obj.id, obj.nombre, obj.usuario.sede);
+    }
 }
 
 export default Usuario;
--- src/view/pages/asignacion-proyectos/asignacion-automatica.jsx
+++ src/view/pages/asignacion-proyectos/asignacion-automatica.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
 import tableStyles from "../../styles/table.module.css";
+import listStyles from "../../styles/list.module.css";
 import Anteproyecto from "../../../controller/anteproyecto";
 import Profesor from "../../../controller/profesor";
 import { asignarAnteproyectosAProfesores } from "../../../controller/asignacion";
 import { loadToast } from "../../components/toast";
+import Modal from "../../components/modal.jsx";
 
 const AsignacionAutomatica = () => {
     const [anteproyectos, setAnteproyectos] = useState([]);
     const [profesores, setProfesores] = useState([]);
     const [calculando, setCalculando] = useState(false);
+    const [asignaciones, setAsignaciones] = useState({asignados: [], noasignados: []});
+    const modalRef = useRef({});
 
     useEffect(() => {
         Anteproyecto.obtenerAsignables().then(setAnteproyectos);
@@ -20,11 +24,16 @@ const AsignacionAutomatica = () => {
     const calcularAsignacion = () => {
         setCalculando(true);
         const asignacion = asignarAnteproyectosAProfesores(anteproyectos, profesores);
+        asignacion.then(resultado => {
+            console.log(resultado);
+            modalRef.open();
+            setAsignaciones(resultado);
+        });
         asignacion.finally(() => setCalculando(false));
         loadToast(
             asignacion,
             "Realizando asignación de anteproyectos...",
-            "Asignación realizada",
+            "Asignación calculada",
             "Error en asignación !!!"
         );
     }
@@ -34,34 +43,42 @@ const AsignacionAutomatica = () => {
             <Button onClick={calcularAsignacion} disabled={calculando}>Inicializar asignación</Button>
             <TablaAnteproyectos anteproyectos={anteproyectos}/>
             <TablaProfesores profesores={profesores}/>
+            <Modal title="Asignación Calculada" modalRef={modalRef}>
+                <ul className={listStyles.list}>
+                    <li>Anteproyectos asignados</li>
+                    {asignaciones.asignados.map((anteproyecto, index) =>
+                        <li>
+                            <ElementoDatosAnteproyecto anteproyecto={anteproyecto}/>
+                            <hr/>
+                            <p><label>Profesor asignado:</label> {anteproyecto.encargado.nombre}</p>
+                        </li>
+                    )}
+                </ul>
+            </Modal>
         </Layout>
     </>;
 }
 
 const TablaAnteproyectos = ({ anteproyectos }) => <>
-    <table className={tableStyles.table}>
-        <thead>
-            <tr className={tableStyles.title}>
-                <th colSpan={4}>Anteproyectos asignables</th>
-            </tr>
-            <tr>
-                <th>Estudiante</th>
-                <th>Sede</th>
-                <th>Empresa</th>
-                <th>Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            {anteproyectos.map((ap, index) =>
-                <tr key={`ap-${index}`} className={tableStyles.tableSpace}>
-                    <td>{ap.estudiante.nombre}</td>
-                    <td>{ap.estudiante.sede}</td>
-                    <td>{ap.nombre}</td>
-                    <td>{ap.estado}</td>
-                </tr>
-            )}
-        </tbody>
-    </table>
+    <ul className={listStyles.list}>
+        <li className={listStyles.title}>Anteproyectos asignables</li>
+        {anteproyectos.map((ap, index) =>
+            <li key={`ap-${index}`}>
+                <ElementoDatosAnteproyecto anteproyecto={ap}/>     
+            </li>
+        )}
+    </ul>
+</>;
+
+const ElementoDatosAnteproyecto = ({ anteproyecto }) => <>
+    <p><label>Estudiante:</label> {anteproyecto.estudiante.nombre}</p>
+    <p><label>Sede:</label> {anteproyecto.estudiante.sede}</p>
+    <p><label>Empresa:</label> {anteproyecto.nombre}</p>
+    <p><label>Estado:</label> {anteproyecto.estado}</p>
+    <p>
+        <label>Encargados anteriores:</label>{" "}
+        {anteproyecto.anteproyectosPerdidos.map(ap => ap.profesor.nombre).join(", ") || "-"}
+    </p>
 </>;
 
 const TablaProfesores = ({ profesores }) => <>
