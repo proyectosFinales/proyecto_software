fdd2094dd28e2260070ad043d5e0be53db864bc5
Mauricio Aguero <mam.121002@gmail.com>
2024-10-19 21:31:49 -0600

    header y footer citas

--- src/view/pages/Citas.js
+++ src/view/pages/Citas.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import '../styles/Citas.css';
 import Modal from '../components/Modal';
 import { supabase } from '../../model/Cliente';
+import Footer from '../components/Footer';
+import Header from '../components/HeaderCoordinador';
 
 const Citas = () => {
   const [fecha, setFecha] = useState('');
@@ -168,177 +170,181 @@ const Citas = () => {
   };
 
   return (
-    <div className="citas-form container">
-      {/* Form */}
-      <div className="row justify-content-center">
-        <div className="col-8">
-          <div className="cita-form row justify-content-center">
-            <div className="col-12">
-              <h2 className="w-100 text-center">Información de la cita</h2>
-            </div>
+    <div>
+      <Header title="Citas" />
+      <div className="citas-form container">
+        {/* Form */}
+        <div className="row justify-content-center">
+          <div className="col-8">
+            <div className="cita-form row justify-content-center">
+              <div className="col-12">
+                <h2 className="w-100 text-center">Información de la cita</h2>
+              </div>
 
-            <div className="col-12">
-              <label>
-                Fecha:
-                <input
-                  type="date"
-                  className="styled-input"
-                  value={fecha}
-                  onChange={(e) => setFecha(e.target.value)}
-                />
+              <div className="col-12">
+                <label>
+                  Fecha:
+                  <input
+                    type="date"
+                    className="styled-input"
+                    value={fecha}
+                    onChange={(e) => setFecha(e.target.value)}
+                  />
+                </label>
+              </div>
+
+              <div className="col-12">
+                <label>
+                  Hora de inicio:
+                  <input
+                    type="time"
+                    className="styled-input"
+                    value={horaInicio}
+                    onChange={(e) => setHoraInicio(e.target.value)}
+                    step="300" // 300 seconds = 5 minutes
+                  />
+                </label>
+              </div>
+
+              <div className="col-12 d-flex justify-content-center">
+                <button className="cita-btn w-50" onClick={handleAsignarCita}>Guardar cita</button>
+              </div>
+
+              <div className="col-12">
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        {/* Table */}
+        <div className="cita-table row justify-content-center">
+          <div className="row justify-content-center mb-3">
+            <h2 className="col-5 w-auto m-0">Lista de citas</h2>
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Día</th>
+                <th>Hora</th>
+                <th>Estudiante</th>
+                <th>Lector 1</th>
+                <th>Lector 2</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {citas.length === 0 ? (
+                <tr>
+                  <td colSpan="6" style={{ textAlign: 'center' }}>
+                    No hay citas asignadas.
+                  </td>
+                </tr>
+              ) : (
+                citas.map((cita) => {
+                  const lector1 = lectores.find((lect) => lect.id === cita.lector1);
+                  const lector2 = lectores.find((lect) => lect.id === cita.lector2);
+                  const estudianteNombre = estudiantes[cita.anteproyectoID]
+
+                  return (
+                    <tr className='cita-row' key={cita.id} onClick={() => handleCitaClick(cita)}>
+                      <td>{formatDateDDMMYYYY(cita.fecha)}</td>
+                      <td>{`${cita.horaInicio} - ${cita.horaFin}`}</td>
+                      <td>{estudianteNombre ? estudianteNombre : 'N/A'}</td>
+                      <td>{lector1 ? lector1.nombre : 'N/A'}</td>
+                      <td>{lector2 ? lector2.nombre : 'N/A'}</td>
+                    </tr>
+                  );
+                })
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Modal */}
+        <Modal show={modal} onClose={() => setModal(false)}>
+          {citaActual && (
+            <>
+              <h2>Editar Cita</h2>
+
+              <label className="label-modal">
+                Estudiante: {estudiantes[citaActual.anteproyectoID] || 'N/A'}
               </label>
-            </div>
 
-            <div className="col-12">
-              <label>
+              <label className="label-modal">
+                Profesor: {profesores[citaActual.anteproyectoID] || 'N/A'}
+              </label>
+
+              <label className="label-modal">
+                Lector 1:
+                <select
+                  name="lector1"
+                  className="styled-input"
+                  value={citaActual.lector1 || ''}
+                  onChange={(e) => {
+                    const updatedAppt = { ...citaActual, lector1: e.target.value || null };
+                    setCitaActual(updatedAppt);
+                  }}
+                >
+                  <option value="">Seleccione un lector</option>
+                  {lectores.map((lector) => (
+                    <option key={lector.id} value={lector.id}>
+                      {lector.nombre}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              <label className="label-modal">
+                Lector 2:
+                <select
+                  name="lector2"
+                  className="styled-input"
+                  value={citaActual.lector2 || ''}
+                  onChange={(e) => {
+                    const updatedAppt = { ...citaActual, lector2: e.target.value || null };
+                    setCitaActual(updatedAppt);
+                  }}
+                >
+                  <option value="">Seleccione un lector</option>
+                  {lectores.map((lector) => (
+                    <option key={lector.id} value={lector.id}>
+                      {lector.nombre}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              <label className="label-modal">
                 Hora de inicio:
                 <input
                   type="time"
                   className="styled-input"
-                  value={horaInicio}
-                  onChange={(e) => setHoraInicio(e.target.value)}
+                  value={citaActual.horaInicio}
+                  onChange={(e) => {
+                    const updatedTime = e.target.value;
+                    const updatedCita = {
+                      ...citaActual,
+                      horaInicio: updatedTime,
+                      horaFin: addOneHour(updatedTime),
+                    };
+                    setCitaActual(updatedCita);
+                  }}
                   step="300" // 300 seconds = 5 minutes
                 />
               </label>
-            </div>
 
-            <div className="col-12 d-flex justify-content-center">
-              <button className="cita-btn w-50" onClick={handleAsignarCita}>Guardar cita</button>
-            </div>
-
-            <div className="col-12">
-              {error && <p style={{ color: 'red' }}>{error}</p>}
-            </div>
-
-          </div>
-        </div>
+              <div className="modal-actions">
+                <button className="cita-btn w-50" onClick={() => updateCita(citaActual)}>Guardar</button>
+                <button className="cita-btn w-50" onClick={() => setModal(false)}>Cancelar</button>
+              </div>
+            </>
+          )}
+        </Modal>
       </div>
-
-      {/* Table */}
-      <div className="cita-table row justify-content-center">
-        <div className="row justify-content-center mb-3">
-          <h2 className="col-5 w-auto m-0">Lista de citas</h2>
-        </div>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Día</th>
-              <th>Hora</th>
-              <th>Estudiante</th>
-              <th>Lector 1</th>
-              <th>Lector 2</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {citas.length === 0 ? (
-              <tr>
-                <td colSpan="6" style={{ textAlign: 'center' }}>
-                  No hay citas asignadas.
-                </td>
-              </tr>
-            ) : (
-              citas.map((cita) => {
-                const lector1 = lectores.find((lect) => lect.id === cita.lector1);
-                const lector2 = lectores.find((lect) => lect.id === cita.lector2);
-                const estudianteNombre = estudiantes[cita.anteproyectoID]
-
-                return (
-                  <tr className='cita-row' key={cita.id} onClick={() => handleCitaClick(cita)}>
-                    <td>{formatDateDDMMYYYY(cita.fecha)}</td>
-                    <td>{`${cita.horaInicio} - ${cita.horaFin}`}</td>
-                    <td>{estudianteNombre ? estudianteNombre : 'N/A'}</td>
-                    <td>{lector1 ? lector1.nombre : 'N/A'}</td>
-                    <td>{lector2 ? lector2.nombre : 'N/A'}</td>
-                  </tr>
-                );
-              })
-            )}
-          </tbody>
-        </table>
-      </div>
-
-      {/* Modal */}
-      <Modal show={modal} onClose={() => setModal(false)}>
-        {citaActual && (
-          <>
-            <h2>Editar Cita</h2>
-
-            <label className="label-modal">
-              Estudiante: {estudiantes[citaActual.anteproyectoID] || 'N/A'}
-            </label>
-
-            <label className="label-modal">
-              Profesor: {profesores[citaActual.anteproyectoID] || 'N/A'}
-            </label>
-
-            <label className="label-modal">
-              Lector 1:
-              <select
-                name="lector1"
-                className="styled-input"
-                value={citaActual.lector1 || ''}
-                onChange={(e) => {
-                  const updatedAppt = { ...citaActual, lector1: e.target.value || null };
-                  setCitaActual(updatedAppt);
-                }}
-              >
-                <option value="">Seleccione un lector</option>
-                {lectores.map((lector) => (
-                  <option key={lector.id} value={lector.id}>
-                    {lector.nombre}
-                  </option>
-                ))}
-              </select>
-            </label>
-
-            <label className="label-modal">
-              Lector 2:
-              <select
-                name="lector2"
-                className="styled-input"
-                value={citaActual.lector2 || ''}
-                onChange={(e) => {
-                  const updatedAppt = { ...citaActual, lector2: e.target.value || null };
-                  setCitaActual(updatedAppt);
-                }}
-              >
-                <option value="">Seleccione un lector</option>
-                {lectores.map((lector) => (
-                  <option key={lector.id} value={lector.id}>
-                    {lector.nombre}
-                  </option>
-                ))}
-              </select>
-            </label>
-
-            <label className="label-modal">
-              Hora de inicio:
-              <input
-                type="time"
-                className="styled-input"
-                value={citaActual.horaInicio}
-                onChange={(e) => {
-                  const updatedTime = e.target.value;
-                  const updatedCita = {
-                    ...citaActual,
-                    horaInicio: updatedTime,
-                    horaFin: addOneHour(updatedTime),
-                  };
-                  setCitaActual(updatedCita);
-                }}
-                step="300" // 300 seconds = 5 minutes
-              />
-            </label>
-
-            <div className="modal-actions">
-              <button className="cita-btn w-50" onClick={() => updateCita(citaActual)}>Guardar</button>
-              <button className="cita-btn w-50" onClick={() => setModal(false)}>Cancelar</button>
-            </div>
-          </>
-        )}
-      </Modal>
+      <Footer />
     </div>
   );
 };
