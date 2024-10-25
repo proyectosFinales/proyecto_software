c5dc4ca706ee8eb2897cbe6754ffb40fabe8e691
aalvaradog <j.a_alvarado99@hotmail.com>
2024-10-24 19:29:13 -0600

    ---

---
+++ netlify/functions/sendMail.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  const { destino, asunto, mensaje } = JSON.parse(event.body);
+
+  const data = JSON.stringify({
+    "Messages": [{
+      "From": {
+        "Email": "proyectosoftware07@gmail.com",
+        "Name": "Zephyr"
+      },
+      "To": [{
+        "Email": destino,
+        "Name": "Zephyr User"
+      }],
+      "Subject": asunto,
+      "TextPart": mensaje
+    }]
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.mailjet.com/v3.1/send',
+    data: data,
+    headers: { 'Content-Type': 'application/json' },
+    auth: {
+      username: '3a4ac386b4ed148b6357eeccac0ab83b',
+      password: '33cbb34c0f2241405b48c81dbccb64d4'
+    },
+  };
+
+  try {
+    await axios(config);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully!" })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
