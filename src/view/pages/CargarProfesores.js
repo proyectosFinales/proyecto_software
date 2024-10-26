bda23cced4962566d3c874381f515c84bdc48b56
Tavo0905 <gperezb2002@gmail.com>
2024-10-26 13:07:58 -0600

    Arreglos editar perfil y userInfo

--- src/view/pages/CargarProfesores.js
+++ src/view/pages/CargarProfesores.js
@@ -3,7 +3,7 @@ import * as XLSX from 'xlsx';
 import { supabase } from '../../model/Cliente';
 import styles from '../styles/CargarProfesores.module.css';
 import Header from '../components/HeaderCoordinador';
-import sendMail from '../../controller/Email';
+import sendMail from '../../controller/email';
 
 const CargarDatos = () => {
   const [excelData, setExcelData] = useState([]);
