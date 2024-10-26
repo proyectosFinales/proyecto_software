1b7bfa70afa6cb9a541f20e2fe6980c7d011c8a0
Roggenrolo <rmcordero.99@gmail.com>
2024-10-26 11:19:12 -0600

    listado de anteproyectos de profesor

--- src/controller/estudiante.js
+++ src/controller/estudiante.js
@@ -1,5 +1,19 @@
 import Usuario from "./usuario";
 
-class Estudiante extends Usuario {}
+class Estudiante extends Usuario {
+    carnet;
+    telefono;
+    
+    constructor(id, nombre, sede, correo, carnet, telefono) {
+        super(id, nombre, sede, correo);
+        this.carnet = carnet;
+        this.telefono = telefono;
+    }
+
+    static from(obj) {
+        if(!obj) return null;
+        return new Estudiante(obj.id, obj.nombre, obj.usuario.sede, obj.usuario.correo, obj.carnet, obj.telefono);
+    }
+}
 
 export default Estudiante;
