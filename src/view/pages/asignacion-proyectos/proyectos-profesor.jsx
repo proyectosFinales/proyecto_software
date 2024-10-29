e73a2f89945c6d12bff7b83e0546f6e2bdc6128e
Roggenrolo <rmcordero.99@gmail.com>
2024-10-29 12:36:06 -0600

    adición de correo a estudiante de anteproyecto

--- src/view/pages/asignacion-proyectos/proyectos-profesor.jsx
+++ src/view/pages/asignacion-proyectos/proyectos-profesor.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Profesor from "../../../controller/profesor";
 import Layout from "../../components/layout";
 import tableStyles from "../../styles/table.module.css";
+import listStyles from "../../styles/list.module.css";
 
 const ProyectosAsignadosProfesor = () => {
     const [anteproyectos, setAnteproyectos] = useState([]);
@@ -13,26 +14,18 @@ const ProyectosAsignadosProfesor = () => {
 
     return <>
         <Layout title="Proyecto asignados a profesor">
-            <table className={tableStyles.table}>
-                <thead>
-                    <tr>
-                        <th>Empresa</th>
-                        <th>Estudiante</th>
-                        <th>Carnet</th>
-                        <th>Teléfono</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {anteproyectos.map((anteproyecto, index) =>
-                        <tr key={`anteproyecto-${index}`} className={tableStyles.tableSpace}>
-                            <td>{anteproyecto.nombreEmpresa}</td>
-                            <td>{anteproyecto.estudiante.nombre}</td>
-                            <td>{anteproyecto.estudiante.carnet}</td>
-                            <td>{anteproyecto.estudiante.telefono}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+            <ul className={listStyles.list}>
+                <li className={listStyles.title}>Anteproyectos asignados</li>
+                {anteproyectos.map((anteproyecto, index) =>
+                    <li key={`anteproyecto-${index}`}>
+                        <p><label>Empresa:</label> {anteproyecto.nombreEmpresa}</p>
+                        <p><label>Estudiante:</label> {anteproyecto.estudiante.nombre}</p>
+                        <p><label>Correo:</label> {anteproyecto.estudiante.correo}</p>
+                        <p><label>Carnet:</label> {anteproyecto.estudiante.carnet}</p>
+                        <p><label>Teléfono:</label> {anteproyecto.estudiante.telefono}</p>
+                    </li>
+                )}
+            </ul>
         </Layout>
     </>;
 }
