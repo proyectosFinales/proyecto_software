c715941b9aca0e709ce8446934a6c7640052a69f
Tavo0905 <gperezb2002@gmail.com>
2024-10-29 22:40:42 -0600

    Validacion de entradas para Agregar usario en gestion

--- src/controller/validarEntradas.js
+++ src/controller/validarEntradas.js
@@ -1,7 +1,6 @@
 
-export async function validateInfo(carnet, tel, email, password) {
+export function validateInfo(carnet, tel, email, password) {
 
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@estudiantec\.cr$/;
     const telRegex = /^[2-9][0-9]{7}$/;
     const carnetRegex = /^[0-9]{10}$/;
 
@@ -9,11 +8,10 @@ export async function validateInfo(carnet, tel, email, password) {
         throw new Error("El carnet no cumple con un formato válido, asegúrese de que el carnet cumpla con el formato de la institución.");
     } else if (!telRegex.test(tel)) {
         throw new Error("El número de teléfono no cumple con un formato válido, asegúrese de que sea un número de teléfono válido.");
-    } else if (!emailRegex.test(email)) {
+    } else if (!validarCorreo(email)) {
         throw new Error("El correo no cumple con un formato válido, asegúrese de ingresar su correo de la institución.");
-    } else if (validarContraseña(password)) {
-        throw new Error("La contraseña no es válida, debe contener al menos 8 caracteres \
-y que mínimo contenga:\n- 1 minúscula\n- 1 mayúscula\n- 1 número\n- 1 caracter especial");
+    } else if (!validarContraseña(password)) {
+        throw new Error("La contraseña no es válida, debe contener al menos 8 caracteres y que mínimo contenga:\n- 1 minúscula\n- 1 mayúscula\n- 1 número\n- 1 caracter especial");
     } else {
         return true;
     }
@@ -23,12 +21,13 @@ y que mínimo contenga:\n- 1 minúscula\n- 1 mayúscula\n- 1 número\n- 1 caract
 export function validarContraseña(contraseña) {
 
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
-    console.log(passwordRegex.test(contraseña));
-    if (!passwordRegex.test(contraseña)) {
-        return true;
-    } else {
-        return false;
-    }
+    return passwordRegex.test(contraseña);
+}
+
+export function validarCorreo(correo) {
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@(estudiantec\.cr|itcr\.ac\.cr)$/;
+    return emailRegex.test(correo);
 }
 
 export default validateInfo;
