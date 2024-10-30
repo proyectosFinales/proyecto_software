ffb59fb239a007a5b8deb8a9794d399158f1854a
Tavo0905 <gperezb2002@gmail.com>
2024-10-29 23:41:05 -0600

    Validar entradas en editar usuario por gestion de perfiles de admin

--- src/controller/validarEntradas.js
+++ src/controller/validarEntradas.js
@@ -1,5 +1,5 @@
 
-export function validateInfo(carnet, tel, email, password) {
+export function validateInfo(carnet, tel, email, password, checkPass = true) {
 
     const telRegex = /^[2-9][0-9]{7}$/;
     const carnetRegex = /^[0-9]{10}$/;
@@ -7,10 +7,11 @@ export function validateInfo(carnet, tel, email, password) {
     if (!carnetRegex.test(carnet)) {
         throw new Error("El carnet no cumple con un formato válido, asegúrese de que el carnet cumpla con el formato de la institución.");
     } else if (!telRegex.test(tel)) {
+        console.log(tel);
         throw new Error("El número de teléfono no cumple con un formato válido, asegúrese de que sea un número de teléfono válido.");
     } else if (!validarCorreo(email)) {
         throw new Error("El correo no cumple con un formato válido, asegúrese de ingresar su correo de la institución.");
-    } else if (!validarContraseña(password)) {
+    } else if (!validarContraseña(password) && checkPass) {
         throw new Error("La contraseña no es válida, debe contener al menos 8 caracteres y que mínimo contenga:\n- 1 minúscula\n- 1 mayúscula\n- 1 número\n- 1 caracter especial");
     } else {
         return true;
