dcc1b4cb930e9e4a176bec8a551d4740c1671dda
Roggenrolo <rmcordero.99@gmail.com>
2024-11-12 16:06:18 -0600

    arreglo de asignación manual de proyecto

--- src/view/pages/asignacion-proyectos/proyectos-profesor.jsx
+++ src/view/pages/asignacion-proyectos/proyectos-profesor.jsx
@@ -1,88 +1,42 @@
 import { useEffect, useState } from "react";
 import listStyles from "../../styles/list.module.css";
-import Header from "../../components/HeaderProfesor";
-import Footer from "../../components/Footer";
 import Styles from "../../styles/proyectos-profesor.module.css";
 import { descargarAnteproyecto } from "../../../controller/DescargarPDF";
-import { supabase } from "../../../model/Cliente";
+import Profesor from "../../../controller/profesor";
+import Layout from "../../components/layout";
+import SidebarProfesor from "../../components/SidebarProfesor";
+import SettingsCoordinador from "../../components/SettingsProfesor";
 
 const ProyectosAsignadosProfesor = () => {
     const [anteproyectos, setAnteproyectos] = useState([]);
 
     useEffect(() => {
-        consultarAnteproyectos();
+        Profesor.fromID(sessionStorage.getItem("token")).then(profesor => {
+            setAnteproyectos(profesor.anteproyectos);
+        });
     }, []);
 
-    async function consultarAnteproyectos() {
-        try {
-            const { data, error } = await supabase.from('anteproyectos')
-                .select(`
-                    id,
-                    sede,
-                    tipoEmpresa,
-                    nombreEmpresa,
-                    actividadEmpresa,
-                    distritoEmpresa,
-                    cantonEmpresa,
-                    provinciaEmpresa,
-                    nombreAsesor,
-                    puestoAsesor,
-                    telefonoContacto,
-                    correoContacto,
-                    nombreHR,
-                    telefonoHR,
-                    correoHR,
-                    tipoEmpresa,
-                    contexto,
-                    justificacion,
-                    sintomas,
-                    impacto,
-                    nombreDepartamento,
-                    tipoProyecto,
-                    observaciones,
-                    estado,
-                    idEstudiante,
-                    estudiantes(id, nombre, carnet, telefono, correo)
-                `)
-                .eq('idEncargado', localStorage.getItem('token'))
-                .eq('semestreActual', 1);
-            if (error) {
-                alert('No se pudieron obtener los anteproyectos');
-                return;
-            }
-            setAnteproyectos(data);
-        } catch (error) {
-            alert('Error al consultar anteproyectos:', error);
-        }
-    }
-
     return (
-        <>
-            <Header title="Proyecto asignados a profesor" />
-            <div className={Styles.lista_proyectos}>
-                <ul className={listStyles.list}>
-                    <li className={listStyles.title}>Anteproyectos asignados</li>
-                    {anteproyectos.map((anteproyecto, index) => {
-                        return (
-                            <li key={`anteproyecto-${index}`}>
-                                <p><label>Empresa:</label> {anteproyecto.nombreEmpresa}</p>
-                                <p><label>Estudiante:</label> {anteproyecto.estudiantes.nombre}</p>
-                                <p><label>Correo:</label> {anteproyecto.estudiantes.correo}</p>
-                                <p><label>Carnet:</label> {anteproyecto.estudiantes.carnet}</p>
-                                <p><label>Teléfono:</label> {anteproyecto.estudiantes.telefono}</p>
-                                <button
-                                    className={Styles.descargar}
-                                    onClick={() => descargarAnteproyecto(anteproyecto)}
-                                >
-                                    Descargar
-                                </button>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-            <Footer />
-        </>
+        <Layout title="Proyecto asignados a profesor" Sidebar={SidebarProfesor} Settings={SettingsCoordinador}>
+            <ul className={listStyles.list}>
+                <li className={listStyles.title}>Anteproyectos asignados</li>
+                {anteproyectos.map((anteproyecto, index) => {
+                    return (
+                        <li key={`anteproyecto-${index}`}>
+                            <p><label>Empresa:</label> {anteproyecto.nombreEmpresa}</p>
+                            <p><label>Estudiante:</label> {anteproyecto.estudiante.nombre}</p>
+                            <p><label>Correo:</label> {anteproyecto.estudiante.correo}</p>
+                            <p><label>Carnet:</label> {anteproyecto.estudiante.carnet}</p>
+                            <p><label>Teléfono:</label> {anteproyecto.estudiante.telefono}</p>
+                            <button
+                                className={Styles.descargar}
+                                onClick={() => descargarAnteproyecto({...anteproyecto, estudiantes: anteproyecto.estudiante})}
+                            >Descargar</button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </Layout>
     );
 };
 
