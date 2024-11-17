7fdb802e7c3eb451cab3afa4a0f02c03fa36c9b0
aalvaradog <j.a_alvarado99@hotmail.com>
2024-11-17 11:33:14 -0600

    correcion ruta

--- src/view/components/SidebarProfesor.js
+++ src/view/components/SidebarProfesor.js
@@ -6,7 +6,7 @@ const SidebarProfesor = ({show}) => {
   return (
     <nav className={show ? styles.sidebar : styles.sidebarHide}>
     <ul>
-        <Link to="/" className={styles.menuItem}><li>Inicio</li></Link>
+        <Link to="/menuProfesor" className={styles.menuItem}><li>Inicio</li></Link>
         <Link to="/proyectos-profesor" className={styles.menuItem}><li>Proyectos</li></Link>
         <Link to="/disponibildad-profesor" className={styles.menuItem}><li>Disponibilidad para defensas</li></Link>
         <Link to="/citas-profesor" className={styles.menuItem}><li>Citas</li></Link>
