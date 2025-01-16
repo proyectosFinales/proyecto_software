82b06bd8b0091ac73549cf0c5f20350e86863cdd
Alonso <106555422+AlonsoNav@users.noreply.github.com>
2025-01-16 12:27:57 -0600

    Add: consult and add events

---
+++ src/controller/Calendario.js
@@ -0,0 +1,25 @@
+import supabase from '../model/supabase';
+
+export const getEventos = async () => {
+  const { data, error } = await supabase
+    .from('Calendario')
+    .select('calendario_id, nombre, fecha_inicio, fecha_fin');
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
+
+export const addEvento = async (evento) => {
+  const { data, error } = await supabase
+    .from('Calendario')
+    .insert([evento])
+    .select('calendario_id, nombre, fecha_inicio, fecha_fin');
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data[0];
+};
