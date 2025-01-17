6d31e0448ffca2591f215d61d7f33217e5a27018
jcardonar <jcardonar@estudiantec.cr>
2025-01-17 11:31:35 -0600

    a

--- src/view/pages/Asignaciones.js
+++ src/view/pages/Asignaciones.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import MenuAsignaciones from '../components/Assignations';
+import Assignations from '../components/Assignations';
 import '../styles/Asignacion.css';
 
 function Asignacion() {
@@ -8,7 +8,7 @@ function Asignacion() {
     <div className="Asig">
       <Header />
       <main>
-        <MenuAsignaciones />
+        <Assignations/>
       </main>
       <footer>
         <p>Instituto Tecnológico de Costa Rica 2024</p>
