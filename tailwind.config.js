18404f3cdada897f1c89e18f7689072ccabcbd4b
jcardonar <jcardonar@estudiantec.cr>
2025-01-19 03:39:08 -0600

    Base tailwind colors for page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azul: '#060953',
+        blanco: '#ffffff',
+        rojo: '#BD3737',
+        gris_oscuro: '#2C2C2C',
+        gris_claro: '#D9D9D9',
+        celeste: '#85d7ff',
+      },
+    },
   },
   plugins: [],
-}
-
+};
