5df5a70930a06de7e5544d02f6cbbf81f2e3f064
jcardonar <jcardonar@estudiantec.cr>
2025-01-19 12:53:49 -0600

    Avance responsive

--- src/view/components/SidebarCoordinador.js
+++ src/view/components/SidebarCoordinador.js
@@ -1,20 +1,72 @@
 /*SidebarCoordinador.js */
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from './Sidebar.module.css';
 
-const SidebarCoordinador = ({show}) => {
+const SidebarCoordinador = ({ show }) => {
   return (
-    <nav className={show ? styles.sidebar : styles.sidebarHide}>
-    <ul>
-        <Link to="/menuCoordinador" className={styles.menuItem}><li>Inicio</li></Link>
-        <Link to="/anteproyectosCoordinador" className={styles.menuItem}><li>Anteproyecto</li></Link>
-        <Link to="/aprobarProyectos" className={styles.menuItem}><li>Proyectos</li></Link>
-        <Link to="/asignaciones" className={styles.menuItem}><li>Asignaciones</li></Link>
-        <Link to="/carga-datos" className={styles.menuItem}><li>Base de datos</li></Link>
-        <Link to="/citasMenu" className={styles.menuItem}><li>Citas y Calendario</li></Link>
-        <Link to="/gestion-perfiles" className={styles.menuItem}><li>Gestionar perfiles</li></Link>
-    </ul>
+    <nav
+      className={`fixed top-0 left-0 h-full bg-gris_oscuro text-blanco p-6 transition-transform transform ${
+        show ? "translate-x-0" : "-translate-x-full"
+      } w-64 shadow-lg z-50`}
+    >
+      <ul className="space-y-4">
+        <li>
+          <Link
+            to="/menuCoordinador"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/anteproyectosCoordinador"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Anteproyecto
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/aprobarProyectos"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Proyectos
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/asignaciones"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Asignaciones
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/carga-datos"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Base de datos
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/citasMenu"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Citas y Calendario
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/gestion-perfiles"
+            className="block p-2 hover:bg-gris_claro rounded"
+          >
+            Gestionar perfiles
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
