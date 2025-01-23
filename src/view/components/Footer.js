ca826531bc6828905adfbe036786933407b7b438
jcardonar <jcardonar@estudiantec.cr>
2025-01-23 00:00:16 -0600

    Responsive footer, deleted useless css

--- src/view/components/Footer.js
+++ src/view/components/Footer.js
@@ -1,11 +1,21 @@
 /*footer.js*/
 import React from "react";
-import './Footer.css'; // Asegúrate de importar el CSS para los estilos
 
 const Footer = () => {
   return (
-    <footer className="login-footer">
-      Instituto Tecnológico de Costa Rica
+    <footer className="fixed bottom-0 left-0 right-0 bg-white shadow-[0_-2px_5px_rgba(0,0,0,0.1)]">
+      <div className="container mx-auto px-4 py-3">
+        <div className="flex flex-col md:flex-row justify-center md:justify-between items-center space-y-2 md:space-y-0">
+          <p className="text-azul text-sm md:text-base">
+            Instituto Tecnológico de Costa Rica
+          </p>
+          <div className="flex items-center space-x-4">
+            <p className="text-gray-600 text-xs md:text-sm">
+              © {new Date().getFullYear()} Todos los derechos reservados
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
