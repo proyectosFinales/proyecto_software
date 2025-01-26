af898d2107ab02b6ea8ae734c79213276e31653e
jcardonar <jcardonar@estudiantec.cr>
2025-01-25 19:19:47 -0600

    reverted gestion perfiles and readded responsive

--- src/controller/Email.js
+++ src/controller/Email.js
@@ -1,14 +1,25 @@
 const sendMail = (destino, asunto, mensaje) => {
+  console.log("sendMail: preparing to send email to", destino);
+
   fetch('/.netlify/functions/sendMail', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ destino: destino, asunto: asunto, mensaje: mensaje }),
+    body: JSON.stringify({ destino, asunto, mensaje }),
   })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log("sendMail: email response received:", data);
+  })
+  .catch(error => {
+    console.error("sendMail: error sending email:", error);
+  });
 };
 
 export default sendMail;
