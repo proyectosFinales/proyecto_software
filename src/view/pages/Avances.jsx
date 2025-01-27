8756f414166215ea48b77dae2aed1ab5e203fd52
Alonso <106555422+AlonsoNav@users.noreply.github.com>
2025-01-27 16:47:06 -0600

    Fix: header on avances

--- src/view/pages/Avances.jsx
+++ src/view/pages/Avances.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Header from '../components/HeaderCoordinador';
+import Header from '../components/HeaderProfesor';
 import Footer from '../components/Footer';
 import '../styles/Calendario.css';
 
