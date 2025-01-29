68d53f6834f75583a001d1cb6dce0b8e57190686
Alonso <106555422+AlonsoNav@users.noreply.github.com>
2025-01-29 12:13:41 -0600

    Add: status change when project approved and when sign up new user

--- src/controller/Semestre.js
+++ src/controller/Semestre.js
@@ -10,3 +10,38 @@ export const fetchSemestres = async () => {
     throw error;
   return data;
 }
+
+export const fetchSemestreActual = async () => {
+  const { data: semestreData, error: semestreError } = await supabase
+    .from('Semestre')
+    .select('semestre_id')
+    .not('calendario_id', 'is', null)
+    .limit(1);
+
+  if (semestreError) {
+    throw new Error(semestreError.message);
+  }
+
+  let semestreId;
+
+  if (semestreData.length > 0) {
+    semestreId = semestreData[0].semestre_id;
+  } else {
+    const { data: maxSemestreData, error: maxSemestreError } = await supabase
+      .from('Semestre')
+      .select('semestre_id')
+      .order('semestre_id', { ascending: false })
+      .limit(1);
+
+    if (maxSemestreError) {
+      throw new Error(maxSemestreError.message);
+    }
+
+    if (maxSemestreData.length > 0) {
+      semestreId = maxSemestreData[0].semestre_id;
+    } else {
+      throw new Error('No se encontró ningún semestre.');
+    }
+  }
+  return semestreId;
+}
